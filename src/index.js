909fcf7408265d438bdb40c4ff0daf5e04c8406d
Chad Lee <git@chadly.net>
2016-09-04 17:18:30 -0500

    Adding create-selector function
    https://github.com/reactjs/reselect#use-memoize-function-from-lodash-for-an-unbounded-cache

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export createReducer from "./create-reducer";
 export fetchOnUpdate from "./fetch-on-update";
 export createAjaxAction from "./create-ajax-action";
+export createSelector from "./create-selector";
