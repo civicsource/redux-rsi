90b7e2f618e622a4cea898dfef614a299193e467
Chad Lee <git@chadly.net>
2016-09-05 16:56:21 -0500

    Turns out we do need the setTimeouts...

--- src/create-ajax-action.js
+++ src/create-ajax-action.js
@@ -6,9 +6,12 @@ export default function createAjaxAction(action, getPromise) {
 		dispatch(action);
 
 		promise.then(response => {
-			dispatch(completed(response, action.payload));
+			// setTimeout here for dispatching success so that any errors that occur during the dispatch are not
+			// handled & potentially swallowed by the promise's catch. A failure should be dispatched only if the
+			// AJAX request iteslf fails, not any error that occurs as a result of processing the AJAX request.
+			setTimeout(() => dispatch(completed(response, action.payload)), 0);
 		}, err => {
-			dispatch(failed(err, action.payload));
+			setTimeout(() => dispatch(failed(err, action.payload)), 0);
 		});
 	};
 
