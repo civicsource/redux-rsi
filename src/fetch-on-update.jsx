civicsource/redux-rsi

commit 914e1e1d44168ea0d012bab6059032839830291a

Use reduce instead of lodash.set

src/fetch-on-update.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { get, set } from "lodash";
+import { get } from "lodash";
 import shallowEqual from "shallowequal";
 
 export default function fetchOnUpdate(fn, ...keys) {
@@ -26,17 +26,16 @@ export default function fetchOnUpdate(fn, ...keys) {
 function mapParams (paramKeys, params) {
 	if (paramKeys.length < 1) return params;
 
-	const result = {};
-
-	paramKeys.forEach(path => {
+	return paramKeys.reduce((result, path) => {
 		const value = get(params, path);
 
 		// move any nested paths to the root of the result
 		// for the purpose of doing a shallow comparison
-		path = path.replace(".", "_");
+		const shallowKey = path.replace(".", "_");
 
-		set(result, path, value);
+		return {
+			...result,
+			[shallowKey]: value
+		};
 	});
-
-	return result;
 }

commit be54a323f3e6fc6df69fb2b40750e954dea3d20f

fetchOnUpdate was not evaluating when to fire correctly
This fixes a bug introduced in 914e1e1d44168ea0d012bab6059032839830291a which caused fetchOnUpdate to never fire the fetch function when params were changing.

Also fixing a bug where fetchOnUpdate would not work correctly with paths nested deeper than 2 levels.

src/fetch-on-update.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { get } from "lodash";
+import { get, set } from "lodash";
 import shallowEqual from "shallowequal";
 
 export default function fetchOnUpdate(fn, ...keys) {
@@ -26,16 +26,17 @@ export default function fetchOnUpdate(fn, ...keys) {
 function mapParams (paramKeys, params) {
 	if (paramKeys.length < 1) return params;
 
-	return paramKeys.reduce((result, path) => {
+	const result = {};
+
+	paramKeys.forEach(path => {
 		const value = get(params, path);
 
 		// move any nested paths to the root of the result
 		// for the purpose of doing a shallow comparison
-		const shallowKey = path.replace(".", "_");
+		path = path.replace(/\./g, "_");
 
-		return {
-			...result,
-			[shallowKey]: value
-		};
+		set(result, path, value);
 	});
+
+	return result;
 }