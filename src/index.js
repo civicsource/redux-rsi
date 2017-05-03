8074045665ed71e3e228183ee0537d39c853c058
Chad Lee <chad.lee@archoninfosys.com>
2017-05-03 11:31:24 -0500

    Remove undocumented createSelector from exports
    Consuming components should just use reselect directly. There is no need for the unbounded cache.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ export createReducer from "./create-reducer";
 export fetchOnUpdate from "./fetch-on-update";
 export asyncifyAction from "./asyncify-action";
 export createAjaxAction from "./create-ajax-action";
-export createSelector from "./create-selector";
 export mergeWithCurrent from "./merge-with-current";
