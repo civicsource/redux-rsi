10a73cc08665f84d22e4e04572f72c16a6c28dd3
Chad Lee <chad.lee@archoninfosys.com>
2017-08-14 14:10:27 -0500

    Revert "Remove undocumented createSelector from exports"
    It turns out we are not quite ready to drop this functionality just yet.
    
    This reverts commit 8074045665ed71e3e228183ee0537d39c853c058.

---
+++ src/create-selector.js
@@ -0,0 +1,11 @@
+// https://github.com/reactjs/reselect#use-memoize-function-from-lodash-for-an-unbounded-cache
+
+import { createSelectorCreator } from "reselect";
+import { memoize } from "lodash";
+
+const hashFn = (...args) => args.reduce(
+	(acc, val) => `${acc}-${JSON.stringify(val)}`,
+	""
+);
+
+export default createSelectorCreator(memoize, hashFn);
--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ export createReducer from "./create-reducer";
 export fetchOnUpdate from "./fetch-on-update";
 export asyncifyAction from "./asyncify-action";
 export createAjaxAction from "./create-ajax-action";
+export createSelector from "./create-selector";
 export mergeWithCurrent from "./merge-with-current";
