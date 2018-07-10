57271563f2ec695a58e5842820c342adab95d5d7
Chad Lee <chad.lee@civicsource.com>
2018-07-09 20:35:29 -0500

    Remove undocumented createSelector from exports
    Consuming components should just use reselect directly. There is no need for the unbounded cache.
    
    This is a redo of 8074045665ed71e3e228183ee0537d39c853c058 which was reverted in 10a73cc08665f84d22e4e04572f72c16a6c28dd3. We *are* ready to remove it now.

--- src/create-selector.js
+++
@@ -1,9 +0,0 @@
-// https://github.com/reactjs/reselect#use-memoize-function-from-lodash-for-an-unbounded-cache
-
-import { createSelectorCreator } from "reselect";
-import { memoize } from "lodash";
-
-const hashFn = (...args) =>
-	args.reduce((acc, val) => `${acc}-${JSON.stringify(val)}`, "");
-
-export default createSelectorCreator(memoize, hashFn);
--- src/index.js
+++ src/index.js
@@ -9,6 +9,4 @@ export mergeWithCurrent from "./merge-with-current";
 export asyncifyAction from "./asyncify-action";
 export createAjaxAction from "./create-ajax-action";
 
-export createSelector from "./create-selector";
-
 export createStore from "./create-store";
